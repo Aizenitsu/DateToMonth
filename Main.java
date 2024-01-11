7b10bf10a1f0725f5b9670cd16bd0c72a296bf7d
Aizenitsu <126737480+Aizenitsu@users.noreply.github.com>
2024-01-11 08:45:50 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -23,8 +23,7 @@ public class Main {
     }
 
     private static String getMonthName(int month) {
-        String[] monthNames = {"January", "February", "March", "April", "May", "June",
-                "July", "August", "September", "October", "November", "December"};
+        String[] monthNames = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
         return monthNames[month - 1];
     }
